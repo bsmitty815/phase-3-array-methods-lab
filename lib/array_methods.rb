06be4c7b6058c09648c9452369b0c70272c6c82b
Brian Smith <brian.bubbam@gmail.com>
2021-07-13 08:36:25 -0500

    commit tree d21e4872fd319bdd067d82df4f5cd2309658bbcb

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+    array.include?(element) #checks if an array includes a certain element
 end
 
 def using_sort(array)
-
+    array.sort # sorts an array
 end
 
 def using_reverse(array)
-
+    array.reverse # reverses an array
 end
 
 def using_first(array)
-
+    array.first # returns first element in an array
 end
 
 def using_last(array)
-
+    array.last # returns last elements in an array
 end
 
 def using_size(array)
-
+    array.size # returns how many elements are in an array
 end
